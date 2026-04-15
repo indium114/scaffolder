7dd7e11aa66beb17fca19c454ce7b76d275aac7b
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-04-15 10:01:20 +0200

    feat: clone repo to tmpdir

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 
 	"charm.land/huh/v2"
 )
 
 var selection string
+
+func clone() {
+	tmpDir, err := os.MkdirTemp("", "scaffolder-*")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	repoDir := filepath.Join(tmpDir, "scaffolder")
+
+	cmd := exec.Command("git", "clone", "https://github.com/indium114/scaffolder", repoDir)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	form := huh.NewForm(
